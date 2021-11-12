096298bcd4f597c9ec5623687bd2ba25b99093ce
expertad630 <ayandas630@gmail.com>
2021-11-12 08:30:30 +0530

    set timout fix

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded',()=>{
     
     setTimeout(()=>{
         document.getElementById('loder').style.display='none';
-    },8000)
+    },5000);
     
     
     
